7db1cf021d7e29f43598515df25846959e0a3760
Roy Tzahor <roy.tzahor1@gmail.com>
2024-04-16 17:43:04 +0300

    update test script and ci yaml

--- run_tests.sh
+++ run_tests.sh
@@ -5,8 +5,5 @@ python3 app.py &
 # Wait a bit for the Flask server to start
 sleep 5
 
-# Run pytest
-pytest test_app.py
-
-# Optionally, kill the Flask app before exiting
-# kill $!
+# Run unittest
+python3 -m unittest test_app.py
